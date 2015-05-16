12b2d47c462238b481f65dd5c4dc58d8ca946b8c
Florian <florian0410@hotmail.fr>
2015-05-16 14:31:17 +0200

    Version HashMap

--- app/src/main/java/com/example/thibaut/testmenualiments/MainActivity.java
+++ app/src/main/java/com/example/thibaut/testmenualiments/MainActivity.java
@@ -6,49 +6,23 @@ import android.content.Context;
 import android.os.Bundle;
 
 import android.widget.GridView;
-
-
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
 
 public class MainActivity extends Activity {
 
     //Liste des aliments
-    private  final String[] nomAlimants={"cabillaud",
-            "calamars",
-            "colin",
-            "crevettes",
-            "daurade",
-            "fletan",
-            "gambas",
-            "haddock",
-            "hareng",
-            "lieu",
-            "maquereau",
-            "merlu",
-            "moules",
-            "oeufs de lump",
-            "Saint Jacques",
-            "sardines",
-            "saumon",
-            "saumon fume",
-            "sole",
-            "surimi",
-            "thon en boite",
-            "truite"};
+    HashMap<String, Integer> HashPoisson = new HashMap<String, Integer>();
 
-    // image associée aux aliments : j'utilise une icône psk j'ai rien d'autre pour l'instant et c'est la même pour tous les produits
-    private int imageAliment=R.drawable.ic_launcher;
+
     // l'ArrayList va stocker les objets aliments
     private ArrayList<Aliment> arrayListAliments;
     //l'adapter view est un GridView pour mettre les deux aliments côte à côte (paramètre deux colonnes voir xml)
     private GridView lv;
     //contexte  de l'activité
     private Context context=MainActivity.this;
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,9 +33,32 @@ public class MainActivity extends Activity {
         setContentView(R.layout.listview);
         //on récupère le gridView
         lv=(GridView)findViewById(R.id.ListViewFishChoice);
-        //instanciation de la liste qui va contenir les objets aliments
+        // Remplissage de la HashMap
+        HashPoisson.put("Cabillaud",R.mipmap.ic_launcher);
+        HashPoisson.put("Calamars",R.mipmap.ic_launcher);
+        HashPoisson.put("Colin",R.mipmap.ic_launcher);
+        HashPoisson.put("Crevettes",R.mipmap.ic_launcher);
+        HashPoisson.put("Daurade",R.mipmap.ic_launcher);
+        HashPoisson.put("Flétan",R.mipmap.ic_launcher);
+        HashPoisson.put("Gambas",R.mipmap.ic_launcher);
+        HashPoisson.put("Haddock",R.mipmap.ic_launcher);
+        HashPoisson.put("Hareng",R.mipmap.ic_launcher);
+        HashPoisson.put("Lieu",R.mipmap.ic_launcher);
+        HashPoisson.put("Maquerau",R.mipmap.ic_launcher);
+        HashPoisson.put("Merlu",R.mipmap.ic_launcher);
+        HashPoisson.put("Moules",R.mipmap.ic_launcher);
+        HashPoisson.put("Oeufs de lump",R.mipmap.ic_launcher);
+        HashPoisson.put("Saint Jacques",R.mipmap.ic_launcher);
+        HashPoisson.put("Sardines",R.mipmap.ic_launcher);
+        HashPoisson.put("Saumon",R.mipmap.ic_launcher);
+        HashPoisson.put("Saumon fumé",R.mipmap.ic_launcher);
+        HashPoisson.put("Sole",R.mipmap.ic_launcher);
+        HashPoisson.put("Surimi",R.mipmap.ic_launcher);
+        HashPoisson.put("Thon en boite",R.mipmap.ic_launcher);
+        HashPoisson.put("Truite",R.mipmap.ic_launcher);
+
         arrayListAliments=new ArrayList<Aliment>();
-        //remplissage de l'arrayListe avec des objets aliments
+        //remplissage de l'arrayList avec des objets aliments
         createArrayList();
         //on crée l'adapter personnalisé et on l'associe à l'adapterView
         lv.setAdapter(new MyAdapter(arrayListAliments,context));
@@ -71,13 +68,12 @@ public class MainActivity extends Activity {
 
       public void createArrayList(){
 
-          int sizeNomAliments=this.nomAlimants.length;
-          for(int i=0; i< sizeNomAliments;i++){
-              /*
-              Création des objets aliments
-               */
-              Aliment nom= new Aliment(imageAliment,nomAlimants[i]);
-              arrayListAliments.add(nom);
+          Iterator<String> keySetIterator = HashPoisson.keySet().iterator();
+
+          while(keySetIterator.hasNext()){
+              String key = keySetIterator.next();
+              Aliment alim = new Aliment(HashPoisson.get(key),key);
+              arrayListAliments.add(alim);
           }
       }
 
